8c5498224ca08d222359f7dcbcd0f45ca5700545
Kyle Julian <38759683+kylejuliandev@users.noreply.github.com>
2025-08-04 22:44:53 +0100

    Inject HttpClient with IHttpClientFactory
    * Ensure API has https url
    
    Signed-off-by: Kyle Julian <38759683+kylejuliandev@users.noreply.github.com>

--- src/Ires.Frontend/Client/ServiceCollectionExtensions.cs
+++ src/Ires.Frontend/Client/ServiceCollectionExtensions.cs
@@ -8,15 +8,18 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddIresClient(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddHttpClient("ires-api", client =>
+        {
+            client.BaseAddress = new Uri(configuration["services:ires-api:https:0"]);
+        });
+
         services.AddScoped<IRequestAdapter, HttpClientRequestAdapter>(provider =>
         {
             var authProvider = new AnonymousAuthenticationProvider();
-            var adapter = new HttpClientRequestAdapter(authProvider)
-            {
-                BaseUrl = configuration["services:ires-api:http:0"]
-            };
+            var httpClient = provider.GetRequiredService<IHttpClientFactory>()
+                .CreateClient("ires-api");
 
-            return adapter;
+            return new HttpClientRequestAdapter(authProvider, httpClient: httpClient);
         });
         services.AddScoped<IresClient>();
 
