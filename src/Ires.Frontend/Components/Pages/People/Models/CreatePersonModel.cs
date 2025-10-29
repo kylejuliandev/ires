8cc4cb822c212c6e3c185d4a90db6c574cb564c5
Kyle Julian <38759683+kylejuliandev@users.noreply.github.com>
2025-10-29 17:24:27 +0000

    Get QuickGrid pagination working and add component for adding a person

---
+++ src/Ires.Frontend/Components/Pages/People/Models/CreatePersonModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ires.Frontend.Components.Pages.People.Models;
+
+public class CreatePersonModel
+{
+    [Required]
+    [MaxLength(50, ErrorMessage = "The maximum length of a given name is 50 characters.")]
+    public string GivenName { get; set; }
+}
