2998fac7f7c51063e248f2dd00485123109d6810
Kyle Julian <38759683+kylejuliandev@users.noreply.github.com>
2025-11-02 15:10:01 +0000

    Add remaining fields for create person form

--- src/Ires.Frontend/Components/Pages/People/Models/CreatePersonModel.cs
+++ src/Ires.Frontend/Components/Pages/People/Models/CreatePersonModel.cs
@@ -7,4 +7,24 @@ public class CreatePersonModel
     [Required]
     [MaxLength(50, ErrorMessage = "The maximum length of a given name is 50 characters.")]
     public string GivenName { get; set; }
+
+    [Required]
+    [MaxLength(100, ErrorMessage = "The maximum length of a family name is 100 characters.")]
+    public string FamilyName { get; set; }
+
+    [MaxLength(50, ErrorMessage = "The maximum length of a nickname is 50 characters.")]
+    public string Nickname { get; set; }
+
+    public CreatePersonModelGender? Gender { get; set; }
+
+    [Required]
+    public DateTime? DateOfBirth { get; set; }
+}
+
+public enum CreatePersonModelGender
+{
+    Male,
+    Female,
+    NonBinary,
+    Other
 }
