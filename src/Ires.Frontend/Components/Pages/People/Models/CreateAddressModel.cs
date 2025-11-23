6b85d36517d5baf5b4c3a59ce88bcd462f436ca3
Kyle Julian <38759683+kylejuliandev@users.noreply.github.com>
2025-11-23 19:47:26 +0000

    feat: Add ability to create addresses

---
+++ src/Ires.Frontend/Components/Pages/People/Models/CreateAddressModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ires.Frontend.Components.Pages.People.Models;
+
+public class CreateAddressModel
+{
+    [Required(ErrorMessage = "You must specify a Street.")]
+    [MaxLength(250, ErrorMessage = "The maximum length of a Street is 250 characters.")]
+    public string Street { get; set; }
+
+    [Required(ErrorMessage = "You must specify a Street.")]
+    [MaxLength(100, ErrorMessage = "The maximum length of a Street is 100 characters.")]
+    public string City { get; set; }
+
+    [Required(ErrorMessage = "You must specify a State.")]
+    [MaxLength(100, ErrorMessage = "The maximum length of a State is 100 characters.")]
+    public string State { get; set; }
+
+    [Required(ErrorMessage = "You must specify a Postal code.")]
+    [MaxLength(20, ErrorMessage = "The maximum length of a Postal code is 20 characters.")]
+    public string PostalCode { get; set; }
+
+    [Required(ErrorMessage = "You must specify a Country.")]
+    [MaxLength(100, ErrorMessage = "The maximum length of a Country is 100 characters.")]
+    public string Country { get; set; }
+}
