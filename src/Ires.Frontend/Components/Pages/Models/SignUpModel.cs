11a7cbcd3eac3241d0720f85c4a26f69f576f287
Kyle Julian <38759683+kylejuliandev@users.noreply.github.com>
2025-11-29 17:38:44 +0000

    feat: Permit users to register

---
+++ src/Ires.Frontend/Components/Pages/Models/SignUpModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ires.Frontend.Components.Pages.Models;
+
+public class SignUpModel
+{
+    [Required]
+    public string Username { get; set; }
+
+    [Required]
+    public string Password { get; set; }
+}
--- src/Ires.Frontend/Services/BasicAuthenticationService.cs
+++ src/Ires.Frontend/Services/BasicAuthenticationService.cs
@@ -17,6 +17,41 @@ public class BasicAuthenticationService
         _dbContext = dbContext;
     }
 
+    public async Task<bool> RegisterAsync(string username, string password)
+    {
+        var httpContext = _httpContextAccessor.HttpContext ?? throw new InvalidOperationException("HttpContext is not available.");
+
+        var existingUser = await _dbContext.Users.FirstOrDefaultAsync(u => u.Username == username);
+        if (existingUser is not null)
+        {
+            return false;
+        }
+
+        var passwordHasher = new PasswordHasher<string>();
+        var user = new User
+        {
+            Username = username,
+            Password = passwordHasher.HashPassword(username, password)
+        };
+        _dbContext.Users.Add(user);
+
+        await _dbContext.SaveChangesAsync();
+
+        var claims = new List<Claim>
+        {
+            new(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new(ClaimTypes.Name, username),
+            new(ClaimTypes.Role, "Admin")
+        };
+        var claimsIdentity = new ClaimsIdentity(claims, "cookie");
+
+        await httpContext.SignInAsync("cookie", new ClaimsPrincipal(claimsIdentity));
+
+        // User is redirected after successful authentication, we
+        // return true to satisfy the compiler.
+        return true;
+    }
+
     public async Task<bool> AuthenticateAsync(string username, string password, bool rememberMe)
     {
         var httpContext = _httpContextAccessor.HttpContext ?? throw new InvalidOperationException("HttpContext is not available.");
